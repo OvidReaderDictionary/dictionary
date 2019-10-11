d110da6cdb9106a265c1a3bbef50f4a75d698f5f
Matt <mattnappo@gmail.com>
2019-10-11 19:36:47 -0400

    Wrote new finder script.

--- src/finder.py
+++ src/finder.py
@@ -1,11 +1,20 @@
 import os, sys, subprocess
 
+def find(search_term):
+    path = os.path.abspath("src/finder.sh")
+
+    out = subprocess.check_output([
+        "sh",
+        path,
+        search_term
+    ])
+
+    # out = os.system("find ./ocr/final/ -type f -exec cat {} + | grep " + sys.argv[1])
+    
+    found = str(out.decode("utf-8"))
+    print(found)
+    
+    return found
+
 if len(sys.argv) == 2:
-
-    # out = subprocess.check_output([
-    #     "sh src/finder.sh",
-    #     sys.argv[1]
-    # ])
-
-    out = os.system("find ./ocr/final/ -type f -exec cat {} + | grep " + sys.argv[1])
-    # print(out)
+    find(sys.argv[1])
